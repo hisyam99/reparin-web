9b4cf11ab092cc851584bb6873b8aae8d50af992
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-05-28 19:22:01 +0700

    Change default language to 'Bahasa Indonesia'

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const host = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : `http://localhost:${port}`;
 
-export const defaultLocale = "en" as const;
+export const defaultLocale = "id" as const;
 export const locales = ["en", "id"] as const;
 
 export const pathnames = {
