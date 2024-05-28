2ca365cecb4ad5cd1e4f3fac7e17a0df8cbc0d5c
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-05-28 19:19:05 +0700

    Change default language to 'Bahasa Indonesia'

--- src/utils/getCurrentLanguage.ts
+++ src/utils/getCurrentLanguage.ts
@@ -1,5 +1,4 @@
-// utils/getCurrentLanguage.ts
 export function getCurrentLanguage(pathname: string): string {
   const pathSegments = pathname.split("/");
-  return pathSegments[1] || "en"; // Default to 'en' if no language segment is found
+  return pathSegments[1] || "id";
 }
