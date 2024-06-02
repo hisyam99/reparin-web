e0fde9e0534c14d471b2ffe6adb27576b9171a9d
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-06-03 01:06:16 +0700

    Fix minor bugs.

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useLocale, useTranslations } from "next-intl";
-import { FormEvent } from "react";
+import { useTranslations } from "next-intl";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
 import LoginForm from "@/components/LoginForm";
@@ -10,15 +11,30 @@ import LoginForm from "@/components/LoginForm";
 export default function Login() {
   const t = useTranslations("Login");
   const router = useRouter();
+  const { data: session, status } = useSession();
 
   const backgroundStyle = {
     minHeight: "100vh",
     padding: "20px",
   };
 
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+
   function handleLoginSuccess() {
     router.refresh();
     router.push("/");
+  }
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    return null; // or you can return a loading spinner or any message
   }
 
   return (
