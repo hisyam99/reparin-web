94bec8f57b14443322ccc80524866fbc885d461b
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-06-03 00:08:31 +0700

    Add PWA's app feature.

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,21 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withNextIntl = createNextIntlPlugin();
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
 
 const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/apis/:path*',
-        destination: 'http://localhost:8080/api/:path*',
+        source: "/apis/:path*",
+        destination: "http://localhost:8080/api/:path*",
       },
     ];
   },
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withPWA(nextConfig));
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,9 +1,55 @@
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
+import type { Metadata, Viewport } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import ClientSessionProvider from "@/components/ClientSessionProvider";
+
+const APP_NAME = "FixITnow";
+const APP_DEFAULT_TITLE = "FixITnow";
+const APP_TITLE_TEMPLATE = "%s - FixITnow";
+const APP_DESCRIPTION = "Cepat, Dekat, Terpercaya.";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
+};
 
 export default async function LocaleLayout({
   children,
