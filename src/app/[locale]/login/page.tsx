a65833696b9dc8eec77d64598651625e4ddcfcf7
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-06-03 23:21:33 +0700

    Redirect to dashboard page if user already login..

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useLocale, useTranslations } from "next-intl";
-import { FormEvent } from "react";
+import { useTranslations } from "next-intl";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 import Link from "next/link";
 
 import LoginForm from "@/components/LoginForm";
@@ -10,6 +11,13 @@ import LoginForm from "@/components/LoginForm";
 export default function Login() {
   const t = useTranslations("Login");
   const router = useRouter();
+  const { data: session, status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/dashboard");
+    }
+  }, [status, router]);
 
   const backgroundStyle = {
     minHeight: "100vh",
@@ -19,6 +27,16 @@ export default function Login() {
   function handleLoginSuccess() {
     router.refresh();
     router.push("/");
+  }
+
+  if (status === "loading" || status === "authenticated") {
+    return (
+      <div style={backgroundStyle}>
+        <div className="container mx-auto">
+          <h1 className="text-3xl font-bold mb-4">Loading...</h1>
+        </div>
+      </div>
+    );
   }
 
   return (
