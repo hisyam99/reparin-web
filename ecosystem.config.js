598e2c6870347c824d47a66c009db3ffe88883e2
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-06-11 16:11:19 +0700

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'bun start'
+  }],
+
+  deploy : {
+    production : {
+      user : 'hisyamkamil99',
+      host : '34.46.7.132',
+      ref  : 'origin/dev',
+      repo : 'git@github.com:hisyam99/sipinter-web.git',
+      path : '/home/hisyamkamil99',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.bashrc && bun install && bun run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
